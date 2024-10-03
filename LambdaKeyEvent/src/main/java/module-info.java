8273637cba906d7fbea452a7fb64d09f29807772
shihanwj <shihanwei495@gmail.com>
2024-10-03 13:51:21 +0200

    Delete LambdaKeyEvent directory

--- LambdaKeyEvent/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module es.ieslosmontecillos.lambdakeyevent {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens es.ieslosmontecillos.lambdakeyevent to javafx.fxml;
-    exports es.ieslosmontecillos.lambdakeyevent;
-}
